ca0505a10c81f4e615d1fe1443a71f0341512426
melloru <pythonubuntu12@gmail.com>
2025-01-26 09:34:53 -0200

    добавил get_all_users

--- application/api/v1/users/views.py
+++ application/api/v1/users/views.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.services import UserService
+from core.db_helper import db_helper
 
 router = APIRouter(
     prefix="/users",
@@ -8,5 +14,8 @@ router = APIRouter(
 
 
 @router.get("/")
-async def test_user():
-    return {"message": "hello world"}
+async def get_all_users(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    users = await UserService.get_all_users(session=session)
+    return users
