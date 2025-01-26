857c238466a0a6fe00be0ce6d4576c18c08c0853
melloru <pythonubuntu12@gmail.com>
2025-01-26 14:17:10 -0200

    новая модель User,созданная с помощью библиотеки fastapi-users

--- application/core/models/user.py
+++ application/core/models/user.py
@@ -1,20 +1,7 @@
-from datetime import datetime
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 
-from sqlalchemy import func, String
-from sqlalchemy.orm import Mapped, mapped_column
-
-from core.models.base import Base
+from core.models import Base
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    username: Mapped[str] = mapped_column(
-        String(21),
-        nullable=False,
-        unique=True,
-    )
-    created_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(),
-        default=datetime.utcnow,
-    )
+class User(SQLAlchemyBaseUserTable[int], Base):
+    pass
