2b454a5f8cb7704b3ec66759018f2cf90bef6ad0
melloru <pythonubuntu12@gmail.com>
2025-01-27 11:28:13 -0200

    create strategy

---
+++ application/core/authentication/strategy.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING, Annotated
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import (
+    DatabaseStrategy,
+)
+
+from core.authentication.dependencies import get_access_token_db
+
+if TYPE_CHECKING:
+    from core.models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+
+
+def get_database_strategy(
+    access_token_db: Annotated[
+        "AccessTokenDatabase[AccessToken]",
+        Depends(get_access_token_db),
+    ],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        database=access_token_db,
+        lifetime_seconds=3600,
+    )
