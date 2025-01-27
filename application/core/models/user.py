e54be1a4bcf53e04aa247793fa9a284687fdb623
melloru <pythonubuntu12@gmail.com>
2025-01-27 08:16:56 -0200

    добавил user transport

--- application/core/models/user.py
+++ application/core/models/user.py
@@ -1,7 +1,16 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from core.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "users"
+
+    @classmethod
+    async def get_user_db(cls, session: AsyncSession):
+        yield SQLAlchemyUserDatabase(session, User)
